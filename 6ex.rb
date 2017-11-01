dc57403354b0e01e43184ce1293d14ce6e2b384f
Omair <omairraza9@gmail.com>
2017-11-01 10:01:53 -0400

    pushing

--- 6ex.rb
+++ 6ex.rb
@@ -1,9 +1,9 @@
 puts "Can you guess the temperature outside in fahrenheit and I will tell you the answer in C"
 fahrenheit = gets.to_i
 
-def conversion(fahrenheit)
-  cell = (fahrenheit - 32) * 5.0/9.0
-end
+  def conversion(fahrenheit)
+    cell = (fahrenheit - 32) * 5.0/9.0
+  end
 
 # p conversion(fahrenheit)
 p "The temperature outside in C is #{conversion(fahrenheit).round(1)}"
